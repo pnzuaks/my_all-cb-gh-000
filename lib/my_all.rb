7b3fb4a71ca71bb5f1ad52023c844b87fc099d29
Patricia Nykaza <pnykaza@calstatela.edu>
2018-04-13 00:07:59 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,6 +7,12 @@ def my_all?(collection)
   while i < collection.length 
   block_return_values << yield(collection[i])
   i += 1 
-
   end
+  
+  if block_return_values.include?(false)
+    false
+  else 
+    true 
+  end
+  
 end
